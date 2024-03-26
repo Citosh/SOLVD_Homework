1128253fd795a120c6bdf1d0bb9cd237c051d358
Citosh <chaplinskyartem@gmail.com>
2024-03-26 16:27:07 +0200

    version 2

--- 1/main.js
+++ 1/main.js
@@ -1,31 +1,13 @@
-
-
-class StringArithmetic {
-
-    constructor(str1){
-        this.str1 = str1;
-    }
-
-    plus(str2){
-        return +this.str1 + +str2;
-    }
-
-    minus(str2){
-        return this.str1 - str2;
-    }
-
-    divide(str2){
-        return ~~(this.str1 / str2) 
-    }
-    
-    multiply(str2){
-        return this.str1 * str2 
-    }
+String.prototype.plus = function (str) {
+    return parseInt(this) + parseInt(str);
+}
+String.prototype.minus = function (str) {
+    return parseInt(this) - parseInt(str);
+}
+String.prototype.divide = function (str) {
+    return (parseInt(this) - parseInt(this) % parseInt(str)) / parseInt(str);
+}
+String.prototype.multiply = function (str) {
+    return parseInt(this) * parseInt(str);
 }
 
-const String = new StringArithmetic('10')
-
-console.log(String.plus('1'))
-console.log(String.minus('2'))
-console.log(String.divide('3'))
-console.log(String.multiply('4'))
