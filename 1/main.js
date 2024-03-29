c6ca39bd2ede641586526095d56366796b1afcea
Citosh <chaplinskyartem@gmail.com>
2024-03-29 10:32:49 +0200

    made all 3 functions acccording to task

--- 1/main.js
+++ 1/main.js
@@ -1,58 +1,97 @@
-String.prototype.toBigInt = function stringToBigInt() {
-    let result = 0n;
-    let  decimalplace= 1n;
-
-    for (let i = this.length - 1; i >= 0; i--) {
-        result += BigInt(this[i]) * decimalplace;
-        decimalplace *= 10n;
+String.prototype.plus = function(str) {
+    let i = this.length - 1;
+    let k = str.length - 1;
+    let result = '';
+    let carry = 0;
+    
+    while (i >= 0 || k >= 0 || carry != 0) {
+        const dg1 = i >= 0 ? parseInt(this[i]) : 0;
+        const dg2 = k >= 0 ? parseInt(str[k]) : 0;
+        const sum = dg1 + dg2 + carry;
+        carry = parseInt(sum / 10);
+        result = (sum % 10) + result;
+        i--;
+        k--;
     }
-
+    
     return result;
 }
 
-String.prototype.plus = function (str) {
-    return `${this.toBigInt() + str.toBigInt()}` 
-}
-
 String.prototype.minus = function (str) {
-    try {
-    const num1 = this.toBigInt()
-    const num2 = str.toBigInt()
-    
-    if (num1<num2) {
-        return new Error( 'Wrong input', {cause :'The second parameter must be greater than the first.'})
-    }
+    let i = this.length - 1;
+    let k = str.length - 1;
+    let result = '';
+    let remind = 0;
 
-    return  `${(num1 - num2)}`
- 
-    } catch (error) {
-       return error
+    while (i >= 0 || remind != 0) {
+        const dg1 = parseInt(this[i]);
+        const dg2 = k >= 0 ? parseInt(str[k]) : 0;
+        let sum = dg1 - dg2 - remind;
+        if (sum < 0) {
+            sum = (10 + dg1) - dg2 - remind
+            remind = 1
+        }else {
+            remind = 0
+        }
+        result = (sum % 10) + result;
+        i--;
+        k--;
     }
-    
+    return result.replace(/^0*(?=[0-9])/g, '');
 }
-String.prototype.divide = function (str) { 
-    try {
-        const num1 = this.toBigInt()
-        const num2 = str.toBigInt()
 
-        return `${( num1 - num1 % num2 ) / num2 }`;
-        
-    } catch (error) {
-        return error
+String.prototype.divide = function (str) {
+
+    let oparatedDigits = '';
+    let result = '';
+    
+    for (let i = 0; i < this.length; i++) {
+        oparatedDigits += this[i];
+        let count = 0;
+
+        while (parseInt(oparatedDigits) >= parseInt(str)) {
+            oparatedDigits = oparatedDigits.minus(str); 
+            count++;
+        }
+        result += count;
     }
+    return result.replace(/^0*(?=[0-9])/g, '');
 }
 
 String.prototype.multiply = function (str) {
-    return `${this.toBigInt() * str.toBigInt()}` 
+    let i = str.length - 1;
+ 
+    let carry = 0; 
+    let partialMiltiplication = 0;
+    let subresult = '';
+    let result = '';
+
+    while ( i >= 0 ) {
+        const dgSTR = i >= 0 ? +str[i] : 0;
+        let k = this.length - 1;
+        subresult = '';
+
+        while (k >= 0){
+
+            const dgTHIS = k >= 0 ? +this[k] : 0;
+
+            partialMiltiplication = dgSTR*dgTHIS + carry;
+            carry = parseInt(partialMiltiplication /10)
+            subresult = (partialMiltiplication % 10) + subresult 
+           
+            k--;
+        } 
+  
+        subresult = subresult + '0'.repeat(str.length - 1 -i)
+        result = result.plus(subresult);
+        
+        i--;
+        
+    }
+    return result
 }
-
-console.log('9007199254740991'.plus('9007199254740991'))
-console.log('999999999999999999999'.minus('999999999999999999999'))
-console.log('999999999999999999999'.divide('10'))
-console.log('999999999999999999999'.multiply('999999999999999999999'))
-
-
-
-
-
-
+    
+console.log(('23546345634563456346534565346'.plus('23546345634563456346534565346')))
+console.log(('23546345634563456346534565346'.minus('23546345634563456346534565346')))
+console.log('23546345634563456346534565346'.multiply('23546345634563456346534565346'))
+console.log('23546345634563456346534565346'.divide('23546345634563456346534565346'))
