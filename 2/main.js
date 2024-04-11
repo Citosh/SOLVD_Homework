5e192d6a1bda99631c7e17a0152007a95d7b676f
Citosh <chaplinskyartem@gmail.com>
2024-04-11 13:41:49 +0300

    1.0.0 version

--- 2/main.js
+++ 2/main.js
@@ -1,11 +1,17 @@
 function addValues(value1, value2) {
-    if (
-      (typeof value1 === "string" || typeof value1 === "number") &&
-      (typeof value2 === "string" || typeof value2 === "number")
-    ) {
-      return value1 + value2;
-    } else {
-      throw new Error("Error! Value1 and Value2 must be strings or numbers.");
+    if(
+      (typeof value1 === 'number' && typeof value2 === 'number') ||
+      (typeof value1 === 'bigint' && typeof value2 === 'bigint') ||
+      (typeof value1 === 'boolean' && typeof value2 === 'boolean')
+    ){
+      return value1 + value2
+    }else if (
+      (typeof value1 === 'string' && typeof value2 === 'string') 
+    ){
+      return value1.concat(value2) 
+    } 
+    else {
+      throw new Error(`cannot perform addition for arguments type : ${typeof value1}, ${typeof value2}`)
     }
   }
 
@@ -17,8 +23,12 @@ function invertBoolean(value1) {
 }
 
 function stringifyValue(value1) {
+  if(typeof value1 === 'symbol')
+    throw new Error('cannot stringify symbol!')
+  else
     return typeof value1 === 'object' ? JSON.stringify(value1) : value1.toString()   
 }
+
 
 
 function convertToNumber(value1) {
@@ -29,32 +39,64 @@ function convertToNumber(value1) {
     } else if (typeof value1 === "string") {
       return parseFloat(value1);
     } else {
-        throw new Error("This value can't convert to number.");
+        throw new Error(`cannot convert ${typeof value1} to number!`);
     }
 }
 
-function convertToBoolean(value1){
-    return Boolean(value1)
-}
 
 
-function coerceToType(value, type) {
+function coerceToType(value1, type) {
     if (type === "boolean") {
-      return convertToBoolean(value);
+      return convertToBoolean(value1);
     } else if (type === "string") {
-      return stringifyValue(value);
+      return stringifyValue(value1);
     } else if (type === "number") {
-      return convertToNumber(value);
-    } else {
-      throw new Error("Unsupported type for coercion.");
+      return convertToNumber(value1);
+    }else if (type === "bigint"){
+      return convertToBigint(value1)
+    }
+
+    else {
+      throw new Error("unsupported type for coercion.");
     }
   }
 
-export default {
+ /* optional */
+
+ function convertToBigint(value1){
+  try {
+    return BigInt(value1)
+  } catch (error) {
+    throw error
+  }
+}
+
+function convertToBoolean(value1){
+  return Boolean(value1)
+}
+
+function convertArrayToObject(value1){
+  if (Array.isArray(value1)){
+    const object = {};
+
+    value1.forEach((element, index) => {
+        object[index] = element;
+    });
+
+    return object;
+  } else{
+    throw new Error(`cannot convert ${typeof value1} to object`)
+  }
+}
+
+
+console.log(convertArrayToObject([1,23,4]))
+export default   {
     addValues,
     invertBoolean,
     stringifyValue,
     convertToNumber,
+    coerceToType,
     convertToBoolean,
-    coerceToType
+    convertArrayToObject
 }
